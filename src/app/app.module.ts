ed90598e56e03d359bc5531a144603f1ef30e181
Brock Sokevitz <bsokevitz@gmail.com>
2020-05-22 14:10:16 -0400

    added routes
    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from "./app.component";
 
 import { LandingComponent } from "./components/landing/landing.component";
 import { AddFormComponent } from "./components/add-form/add-form.component";
+import { SearchComponent } from './search/search.component';
+import { ModifyComponent } from './modify/modify.component';
 
 @NgModule({
-  declarations: [AppComponent, LandingComponent, AddFormComponent],
+  declarations: [AppComponent, LandingComponent, AddFormComponent, SearchComponent, ModifyComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
